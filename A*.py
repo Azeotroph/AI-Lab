1b213852fa49f7fc5e5d66dfae5a60169ca2262c
Azeotroph <118718128+Azeotroph@users.noreply.github.com>
2023-12-20 15:07:21 +0530

    Update A*.py

--- A*.py
+++ A*.py
@@ -1,119 +1,99 @@
-class Node:
-    def __init__(self,data,level,fval):
-        """ Initialize the node with the data, level of the node and the calculated fvalue """
-        self.data = data
-        self.level = level
-        self.fval = fval
+import heapq
 
-    def generate_child(self):
-        """ Generate child nodes from the given node by moving the blank space
-            either in the four directions {up,down,left,right} """
-        x,y = self.find(self.data,'_')
-        """ val_list contains position values for moving the blank space in either of
-            the 4 directions [up,down,left,right] respectively. """
-        val_list = [[x,y-1],[x,y+1],[x-1,y],[x+1,y]]
-        children = []
-        for i in val_list:
-            child = self.shuffle(self.data,x,y,i[0],i[1])
-            if child is not None:
-                child_node = Node(child,self.level+1,0)
-                children.append(child_node)
-        return children
-        
-    def shuffle(self,puz,x1,y1,x2,y2):
-        """ Move the blank space in the given direction and if the position value are out
-            of limits the return None """
-        if x2 >= 0 and x2 < len(self.data) and y2 >= 0 and y2 < len(self.data):
-            temp_puz = []
-            temp_puz = self.copy(puz)
-            temp = temp_puz[x2][y2]
-            temp_puz[x2][y2] = temp_puz[x1][y1]
-            temp_puz[x1][y1] = temp
-            return temp_puz
-        else:
-            return None
-            
+class PuzzleNode:
+    def __init__(self, state, parent=None, g=0, h=0):
+        self.state = state
+        self.parent = parent
+        self.g = g  # cost from start node
+        self.h = h  # heuristic value
+        self.f = self.g + self.h  # total cost
 
-    def copy(self,root):
-        """ Copy function to create a similar matrix of the given node"""
-        temp = []
-        for i in root:
-            t = []
-            for j in i:
-                t.append(j)
-            temp.append(t)
-        return temp    
-            
-    def find(self,puz,x):
-        """ Specifically used to find the position of the blank space """
-        for i in range(0,len(self.data)):
-            for j in range(0,len(self.data)):
-                if puz[i][j] == x:
-                    return i,j
+    def __lt__(self, other):
+        return self.f < other.f
 
+def manhattan_distance(state, goal_state):
+    distance = 0
+    for i in range(3):
+        for j in range(3):
+            if state[i][j] != goal_state[i][j] and state[i][j] != 0:
+                x, y = divmod(goal_state[i][j], 3)
+                distance += abs(x - i) + abs(y - j)
+    return distance
 
-class Puzzle:
-    def __init__(self,size):
-        """ Initialize the puzzle size by the specified size,open and closed lists to empty """
-        self.n = size
-        self.open = []
-        self.closed = []
+def get_blank_position(state):
+    for i in range(3):
+        for j in range(3):
+            if state[i][j] == 0:
+                return i, j
 
-    def accept(self):
-        """ Accepts the puzzle from the user """
-        puz = []
-        for i in range(0,self.n):
-            temp = input().split(" ")
-            puz.append(temp)
-        return puz
+def get_neighbors(node):
+    i, j = get_blank_position(node.state)
+    neighbors = []
 
-    def f(self,start,goal):
-        """ Heuristic Function to calculate hueristic value f(x) = h(x) + g(x) """
-        return self.h(start.data,goal)+start.level
+    for x, y in ((i+1, j), (i-1, j), (i, j+1), (i, j-1)):
+        if 0 <= x < 3 and 0 <= y < 3:
+            neighbor_state = [row[:] for row in node.state]
+            neighbor_state[i][j], neighbor_state[x][y] = neighbor_state[x][y], neighbor_state[i][j]
+            neighbors.append(PuzzleNode(neighbor_state, parent=node))
 
-    def h(self,start,goal):
-        """ Calculates the different between the given puzzles """
-        temp = 0
-        for i in range(0,self.n):
-            for j in range(0,self.n):
-                if start[i][j] != goal[i][j] and start[i][j] != '_':
-                    temp += 1
-        return temp
-        
+    return neighbors
 
-    def process(self):
-        """ Accept Start and Goal Puzzle state"""
-        print("Enter the start state matrix \n")
-        start = self.accept()
-        print("Enter the goal state matrix \n")        
-        goal = self.accept()
+def a_star_search(initial_state, goal_state, heuristic):
+    initial_node = PuzzleNode(initial_state, g=0, h=heuristic(initial_state, goal_state))
+    goal_node = PuzzleNode(goal_state)
 
-        start = Node(start,0,0)
-        start.fval = self.f(start,goal)
-        """ Put the start node in the open list"""
-        self.open.append(start)
-        print("\n\n")
-        while True:
-            cur = self.open[0]
-            print("")
-            print("  | ")
-            print("  | ")
-            print(" \\\'/ \n")
-            for i in cur.data:
-                for j in i:
-                    print(j,end=" ")
-                print("")
-            """ If the difference between current and goal node is 0 we have reached the goal node"""
-            if(self.h(cur.data,goal) == 0):
-                break
-            for i in cur.generate_child():
-                i.fval = self.f(i,goal)
-                self.open.append(i)
-            self.closed.append(cur)
-            del self.open[0]
+    if initial_state == goal_state:
+        return [initial_state]
 
-            """ sort the opne list based on f value """
-            self.open.sort(key = lambda x:x.fval,reverse=False)
+    priority_queue = [initial_node]
+    visited_states = set()
 
-puz = Puzzle(3)
-puz.process()
+    while priority_queue:
+        current_node = heapq.heappop(priority_queue)
+
+        if current_node.state == goal_state:
+            path = [current_node.state]
+            while current_node.parent:
+                current_node = current_node.parent
+                path.append(current_node.state)
+            path.reverse()
+            return path
+
+        visited_states.add(tuple(map(tuple, current_node.state)))
+
+        neighbors = get_neighbors(current_node)
+        for neighbor in neighbors:
+            if tuple(map(tuple, neighbor.state)) not in visited_states:
+                neighbor.g = current_node.g + 1
+                neighbor.h = heuristic(neighbor.state, goal_state)
+                neighbor.f = neighbor.g + neighbor.h
+                heapq.heappush(priority_queue, neighbor)
+
+    return None
+
+# Example usage:
+initial_state = [
+    [1, 2, 3],
+    [4, 0, 5],
+    [6, 7, 8]
+]
+
+goal_state = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 0]
+]
+
+# Heuristic function (Manhattan distance)
+heuristic_function = manhattan_distance
+
+path = a_star_search(initial_state, goal_state, heuristic_function)
+
+if path:
+    print("Solution found:")
+    for state in path:
+        for row in state:
+            print(row)
+        print()
+else:
+    print("No solution found.")
